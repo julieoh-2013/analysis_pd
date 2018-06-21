a87c6d9c5a742b26b0de350d07e186915e53bf58
julie-2013 <julieoh2013@gmail.com>
2018-06-21 21:26:02 +0900

    commit after modification

--- analyze/__init__.py
+++ analyze/__init__.py
@@ -1,2 +1,3 @@
 from .analyzer import analysis_correlation
 from .analyzer import analysis_correlation_by_tourspot
+
--- analyze/analyzer.py
+++ analyze/analyzer.py
@@ -27,7 +27,8 @@ def analysis_correlation(resultfiles):
         # date count_foreigner country_name  visit_count
         x = list(merge_table['visit_count'])
         y = list(merge_table['count_foreigner'])
-        country_name = foreignvisitor_table['country_name'].unique().item(0)
+        print(foreignvisitor_table['country_name'].unique())
+        country_name = foreignvisitor_table['country_name'].unique().item(0)#<class 'numpy.ndarray'>.item() #ndarray.item(*args)
 
         #상관계수추출 scipy설치
         r = correlation_coefficient(x,y)
@@ -42,16 +43,14 @@ def analysis_correlation(resultfiles):
     return results
 
 
-
 def analysis_correlation_by_tourspot(resultfiles):
-
     with open(resultfiles['tourspot_visitor'],'r',encoding='utf-8') as infile :
         json_data = json.loads(infile.read())
     tourspotvisitor_table = pd.DataFrame(json_data, columns=['tourist_spot','date','count_foreigner'])
     tourist_spots = tourspotvisitor_table['tourist_spot'].unique()
 
     results = []
-    for spot in tourist_spots :
+    for spot in tourist_spots:
         data = {'tourspot': spot}
         s = tourspotvisitor_table['tourist_spot'] == spot
         temp_table = tourspotvisitor_table[s]
@@ -69,9 +68,7 @@ def analysis_correlation_by_tourspot(resultfiles):
                                    left_index=True,
                                    right_index=True
                                    )
-            print(merge_table)
             country_name = foreignvisitor_table['country_name'].unique().item(0)
-
             # 상관계수추출 scipy설치
             x = list(merge_table['count_foreigner'])
             y = list(merge_table['visit_count'])
@@ -81,25 +78,30 @@ def analysis_correlation_by_tourspot(resultfiles):
 
         results.append(data)
 
-    return results
-    '''
+    graph_table = pd.DataFrame(results, columns=['tourspot', 'r_중국', 'r_일본', 'r_미국', ])
+    graph_table = graph_table.set_index('tourspot')
+    graph_table.plot(kind='bar', rot = 70)
+    plt.show()
+
+'''
     [{'tourspot': '창덕궁', 'r_중국': -0.05787996838309703, 'r_일본': 0.18113398877560832, 'r_미국': 0.15157690000865773}, 
     {'tourspot': '경복궁', 'r_중국': -0.8435333683208608, 'r_일본': -0.6908586912392769, 'r_미국': -0.8041107208313881}, 
     {'tourspot': '창경궁', 'r_중국': 0.3302835585547996, 'r_일본': 0.1897358329486392, 'r_미국': 0.4564453800391374}, 
-    '''
-    #json 로딩, 머지, 상관계수
 
-    #장소별로 방문자수와 일본인 입국자수와 상관계수 구하기 장소별 상관계수3개나옴
-    #dataframe = 안에서 머지
-    # tourspot r_중국  r_일본  r_미국 중국입국자수 일본입국자수 미국입국자수 중국방문자수....
-    #  경복궁     0.2  0.33    0.88
-    #  경복궁     0.33  0.32    0.22  (딕셔너리 리스트에 넣어서)
-    '''
-    graph_table = pd.DataFrame(result_analysis, colums=['tourspot','r_중국','r_일본','r_미국',])
-    graph_table= graph_table.set_index('tourspot')
-    graph_table.plot(kind='bar')
-    plt.show()
-    '''
+
+#json 로딩, 머지, 상관계수
+
+#장소별로 방문자수와 일본인 입국자수와 상관계수 구하기 장소별 상관계수3개나옴
+#dataframe = 안에서 머지
+# tourspot r_중국  r_일본  r_미국 중국입국자수 일본입국자수 미국입국자수 중국방문자수....
+#  경복궁     0.2  0.33    0.88
+#  경복궁     0.33  0.32    0.22  (딕셔너리 리스트에 넣어서)
+
+graph_table = pd.DataFrame(result_analysis, colums=['tourspot','r_중국','r_일본','r_미국',])
+graph_table= graph_table.set_index('tourspot')
+graph_table.plot(kind='bar')
+plt.show()
+'''
 
 def correlation_coefficient(x, y):
     n = len(x)
